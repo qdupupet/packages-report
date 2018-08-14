c76ba894d53629304f047379c22bd221f59b9553
qdupupet <QDupupet45@MassMutual.com>
2018-08-14 10:32:43 -0400

    challenge 1

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,18 +1,22 @@
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
-
 ## keep the variables
 ##   * Package
 ##   * LibPath
 ##   * Version
 ##   * Priority
 ##   * Built
-
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+library(tidyverse)
+ipt <- installed.packages() %>%
+  data.frame() %>%
+  select(Package, LibPath, Version, Priority, Built) %>%
+  write.csv('data/installed-packages.csv')
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
