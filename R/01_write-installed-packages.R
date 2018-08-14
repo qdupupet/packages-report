58e63165632c25ef40334dae1aaeb3157fa09bc5
qdupupet <QDupupet45@MassMutual.com>
2018-08-14 11:32:15 -0400

    crossed out prompts in challenge 1

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,16 +1,16 @@
-## deja vu from yesterday!
+## ~deja vu from yesterday!~
 
-## create a data frame of your installed packages
-## keep the variables
-##   * Package
-##   * LibPath
-##   * Version
-##   * Priority
-##   * Built
-## write it to data/installed-packages.csv
-## YES overwrite the file that is there now
-## that came from me (Jenny)
-## it an example of what yours should look like
+## ~create a data frame of your installed packages~
+## ~keep the variables~
+##  ~ * Package~
+##  ~ * LibPath~
+##  ~ * Version~
+##  ~ * Priority~
+##  ~ * Built~
+## ~write it to data/installed-packages.csv~
+## ~YES overwrite the file that is there now~
+## ~that came from me (Jenny)~
+## ~it an example of what yours should look like~
 
 library(tidyverse)
 ipt <- installed.packages() %>%
@@ -18,5 +18,5 @@ ipt <- installed.packages() %>%
   select(Package, LibPath, Version, Priority, Built) %>%
   write.csv('data/installed-packages.csv')
 
-## when this script works, stage & commit it and the csv file
+## ~when this script works, stage & commit it and the csv file~
 ## PUSH!
